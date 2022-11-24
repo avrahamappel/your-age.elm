d7fa9b13f3ffe568cec3d0a9a62aa1c89392384a
Avraham Appel <aappel248@gmail.com>
2022-11-23 23:20:55 -0500

    ELm skeleton

--- shell.nix
+++ shell.nix
@@ -4,5 +4,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     # Dependencies
     elmPackages.elm
+    elmPackages.elm-format
   ];
 }
--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,78 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (h1, text)
+import Html exposing (div, input, label, p, text)
+import Html.Attributes exposing (for, name, type_)
 
-main : Program () () msg
-main = Browser.sandbox { init = (), update = update, view = view }
 
-update : a -> b -> ()
-update _ _ = ()
+type alias YourBirthday =
+    { name : String, birthday : Maybe String }
 
-view : a -> Html.Html msg
-view _ = h1 [] [text "Hello World!"]
+
+main : Program () YourBirthday msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+init : YourBirthday
+init =
+    { name = "", birthday = Nothing }
+
+
+update : a -> b -> b
+update _ yb =
+    yb
+
+
+view : YourBirthday -> Html.Html msg
+view model =
+    let
+        form =
+            [ p [] [ text "Type your name and birthday" ]
+            , label [ for "name" ] [ text "Name" ]
+            , input [ name "name" ] []
+            , label [ for "birthday" ] [ text "Birthday" ]
+            , input [ type_ "date", name "birthday" ] []
+            ]
+
+        years =
+            ""
+
+        months =
+            ""
+
+        days =
+            ""
+
+        hours =
+            ""
+
+        minutes =
+            ""
+
+        seconds =
+            ""
+
+        output =
+            [ p [] [ text ("Hello " ++ model.name ++ "!") ]
+            , p [] [ text "You are:" ]
+            , p [] [ text (years ++ " years old") ]
+            , p [] [ text (months ++ " months old") ]
+            , p [] [ text (days ++ " days old") ]
+            , p [] [ text (hours ++ " hours old") ]
+            , p [] [ text (minutes ++ " minutes old") ]
+            , p [] [ text (seconds ++ " seconds old") ]
+            ]
+    in
+    div []
+        (case model.birthday of
+            Just _ ->
+                if not (String.isEmpty model.name) then
+                    form ++ output
+
+                else
+                    form
+
+            Nothing ->
+                form
+        )
